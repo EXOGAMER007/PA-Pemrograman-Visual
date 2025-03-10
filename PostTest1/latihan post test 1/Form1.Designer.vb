6c43cc4085590c8623bceeb689942f29103a274f
EXOGAMER007 <daffa8110@gmail.com>
2025-03-10 21:01:51 +0800

    a

---
+++ PostTest1/latihan post test 1/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(151, 120)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(68, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(151, 176)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(72, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "IP Komulatif"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.AccessibleRole = AccessibleRole.None
+        TextBox1.ImeMode = ImeMode.NoControl
+        TextBox1.Location = New Point(263, 120)
+        TextBox1.Margin = New Padding(3, 2, 3, 2)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(170, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(263, 145)
+        Button1.Margin = New Padding(3, 2, 3, 2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(82, 22)
+        Button1.TabIndex = 3
+        Button1.Text = "Tambah"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(351, 145)
+        Button2.Margin = New Padding(3, 2, 3, 2)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(82, 22)
+        Button2.TabIndex = 4
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.AccessibleRole = AccessibleRole.None
+        TextBox2.Enabled = False
+        TextBox2.ImeMode = ImeMode.NoControl
+        TextBox2.Location = New Point(263, 176)
+        TextBox2.Margin = New Padding(3, 2, 3, 2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(170, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(263, 215)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 15)
+        Label3.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(700, 338)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(3, 2, 3, 2)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ PostTest1/latihan post test 1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim IP, IPK, Jumlah As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        IP += TextBox1.Text
+        Jumlah += 1
+        IPK = IP / Jumlah
+        TextBox2.Text = IPK
+        TextBox1.Text = ""
+        If IPK >= 3.1 Then
+            Label3.Text = "Sangat Memuaskan"
+        ElseIf IPK >= 2.76 And IPK <= 3.0 Then
+            Label3.Text = "Memuaskan"
+        ElseIf IPK >= 2.0 And IPK <= 2.75 Then
+            Label3.Text = "Cukup"
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        IP = 0
+        IPK = 0
+        TextBox2.Text = ""
+        Label3.Text = ""
+    End Sub
+End Class
