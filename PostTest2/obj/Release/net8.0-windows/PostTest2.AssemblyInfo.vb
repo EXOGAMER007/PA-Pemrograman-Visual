c8007b37759fbc41f5f5a21573f265ce33ee1390
EXOGAMER007 <daffa8110@gmail.com>
2025-03-17 22:58:06 +0800

    test

--- PostTest2/obj/Release/net8.0-windows/PostTest2.AssemblyInfo.vb
+++ PostTest2/obj/Release/net8.0-windows/PostTest2.AssemblyInfo.vb
@@ -14,7 +14,7 @@ Option Explicit On
 Imports System
 Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("PostTest2"),  _
- Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
+ Assembly: System.Reflection.AssemblyConfigurationAttribute("Release"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
  Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70707990f2565c76e877ad9533df48e414057826"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("PostTest2"),  _
